e9e15e6fec495ac2b8cb4a6a1edd6142a6b7a854
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2020-09-29 13:08:29 -0300

    Criando pagina de Signup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,53 +3,47 @@ import { NavController, IonicPage } from 'ionic-angular';
 import { MenuController } from 'ionic-angular/components/app/menu-controller';
 import { CredenciaisDTO } from '../../models/credenciais.dto';
 import { AuthService } from '../../services/auth.service';
-
 @IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-creds : CredenciaisDTO = {
-  email:"",
-  senha: ""
-
-}
-
-
-constructor(
-  public navCtrl: NavController, 
-  public menu: MenuController,
-  public auth: AuthService) {
-
-}
-
+  creds : CredenciaisDTO = {
+    email: "",
+    senha: ""
+  };
+  constructor(
+    public navCtrl: NavController, 
+    public menu: MenuController,
+    public auth: AuthService) {
+  }
   ionViewWillEnter() {
-  this.menu.swipeEnable(false);
+    this.menu.swipeEnable(false);
   }
-
-  ionViewDidLeave() {
-  this.menu.swipeEnable(true);
+    
+  ionViewWillLeave() {
+    this.menu.swipeEnable(true);
   }
-
   ionViewDidEnter() {
     this.auth.refreshToken()
-    .subscribe(response =>{
-      this.auth.successfullLogin(response.headers.get('Authorization'));
-      this.navCtrl.setRoot('CategoriasPage');
-    },
-    error => {});
+      .subscribe(response => {
+        this.auth.successfullLogin(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriasPage');
+      },
+      error => {});  
   }
-
   login() {
     this.auth.authenticate(this.creds)
-    .subscribe(response =>{
-      this.auth.successfullLogin(response.headers.get('Authorization'));
-      this.navCtrl.setRoot('CategoriasPage');
-    },
-    error => {});
-   
-  
+      .subscribe(response => {
+        this.auth.successfullLogin(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriasPage');
+      },
+      error => {});    
+  }
+
+  signup() {
+    console.log("passou");
+    this.navCtrl.push('SignupPage');
   }
 }
