10df00b4629b5a37e233c97b6ca79d9399cb5c25
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2020-11-12 17:09:28 -0300

    configuracao do heroku

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage,  NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
-import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
+import { ProdutoService } from '../../services/domain/produto.service';
 import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
 
 @IonicPage()
@@ -12,12 +12,13 @@ import { LoadingController } from 'ionic-angular/components/loading/loading-cont
 })
 export class ProdutosPage {
 
-  items : ProdutoDTO[];
+  items : ProdutoDTO[] = [];
   page : number = 0;
 
-  constructor(public navCtrl: NavController,
-     public navParams: NavParams,
-     public produtoService: ProdutoService,
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public produtoService: ProdutoService,
     public loadingCtrl: LoadingController) {
   }
 
@@ -26,39 +27,38 @@ export class ProdutosPage {
   }
 
   loadData() {
-    let categoria_id = this.navParams.get('categoria_id')
+    let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id, this.page, 10)
-   .subscribe(response => {
-    let start = this.items.length;
-    this.items = this.items.concat(response['content']);
-    let end = this.items.length - 1;
-     loader.dismiss();
-     console.log(this.page);
-     console.log(this.items);
-     this.loadImageUrls(start, end);
-   },
-   error => {
-    loader.dismiss();
-  });
-
-
+      .subscribe(response => {
+        let start = this.items.length;
+        this.items = this.items.concat(response['content']);
+        let end = this.items.length - 1;
+        loader.dismiss();
+        console.log(this.page);
+        console.log(this.items);
+        this.loadImageUrls(start, end);
+      },
+      error => {
+        loader.dismiss();
+      });
   }
-
 
   loadImageUrls(start: number, end: number) {
     for (var i=start; i<=end; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
-          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}.jpg`;
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
         },
         error => {});
     }
   }  
+
   showDetail(produto_id : string) {
     this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
   }
+
   presentLoading() {
     let loader = this.loadingCtrl.create({
       content: "Aguarde..."
@@ -66,6 +66,7 @@ export class ProdutosPage {
     loader.present();
     return loader;
   }
+
   doRefresh(refresher) {
     this.page = 0;
     this.items = [];
@@ -74,6 +75,7 @@ export class ProdutosPage {
       refresher.complete();
     }, 1000);
   }
+
   doInfinite(infiniteScroll) {
     this.page++;
     this.loadData();
